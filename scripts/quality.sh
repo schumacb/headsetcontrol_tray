db33f8bd1ba620125e6b715d830353a7af6c1666
Bastian Schumacher <Bastian.Schumacher@gmx.de>
2025-06-10 14:50:44 +0200

    Added print statements

--- scripts/quality.sh
+++ scripts/quality.sh
@@ -1,10 +1,19 @@
 #! /bin/bash
 
+echo ===== running pytest =====
 uv run pytest
+echo ===== running mypy =====
 uv run mypy .
+echo ===== running ruff check src =====
 uv run ruff check src
+echo ===== running ruff check tests =====
 uv run ruff check tests
+echo ===== running vulture src =====
 uv run vulture src
+echo ===== running vulture tests =====
 uv run vulture tests
+echo ===== running radon cc . -a -s =====
 uv run radon cc . -a -s
+echo ===== running radon mi . -s =====
 uv run radon mi . -s
+echo ===== FINISHED =====
